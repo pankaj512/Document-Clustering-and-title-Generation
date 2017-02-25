d4b1f4a3f6243618e108c6728dc2a0b02ae36122
PANKAJ KUMAR <PANKAJ KUMAR>
2017-02-25 16:56:55 +0530

    add after term extraction

---
+++ mainApp.py
@@ -0,0 +1,9 @@
+# This file contain code for GUI of application and function calling
+# TODO list
+# 1. Create GUI for this that take text and input and classify that as one of mention category.
+# 2. Output should be shown at GUI itself.
+
+
+
+
+
---
+++ preprocessing/test.py
@@ -0,0 +1,3 @@
+from nltk.corpus import stopwords
+stop = set(stopwords.words('english'))
+print(stop)
