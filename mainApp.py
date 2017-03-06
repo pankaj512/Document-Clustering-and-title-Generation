92d7261c815237950e44a46af3a5c8d963c27ac6
YogeshSaini292 <yogeshsaini292@gmail.com>
2017-03-05 21:53:17 -0800

    Main file Updated

--- mainApp.py
+++ mainApp.py
@@ -1,6 +1,6 @@
 # This file contain code for GUI of application and function calling
 # TODO list
-# 1. Create GUI for this that take text and input and classify that as one of mention category.
+# 1. Create GUI for this that take input and classify that as one of mention category.
 # 2. Output should be shown at GUI itself.
 import tkinter as tk
 import pygubu
